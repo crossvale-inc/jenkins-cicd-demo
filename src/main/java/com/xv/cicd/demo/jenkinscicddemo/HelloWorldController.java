2df4d1d9b4b32ab5c7002fac87976a67b461c886
mrivasxvale <mrivas@crossvale.com>
2021-11-22 14:32:51 +0100

    Update cicd resources

---
+++ src/main/java/com/xv/cicd/demo/jenkinscicddemo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.xv.cicd.demo.jenkinscicddemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+
+}
