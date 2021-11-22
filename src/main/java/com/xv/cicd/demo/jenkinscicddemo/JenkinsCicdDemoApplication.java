2e9ef83240758f2669f082b9a30d851d589e7f30
mrivasxvale <mrivas@crossvale.com>
2021-11-22 10:00:43 +0100

    Initial commit

---
+++ src/main/java/com/xv/cicd/demo/jenkinscicddemo/JenkinsCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xv.cicd.demo.jenkinscicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xv/cicd/demo/jenkinscicddemo/JenkinsCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xv.cicd.demo.jenkinscicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
